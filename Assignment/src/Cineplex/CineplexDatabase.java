7dd7287fb2eac3fe5cdd647245d6d318e32dc538
AssignmentNTU <assignmentntu@yahoo.com>
2015-11-12 17:23:33 +0800

    Update CineplexDatabase.java

--- Assignment/src/Cineplex/CineplexDatabase.java
+++ Assignment/src/Cineplex/CineplexDatabase.java
@@ -18,12 +18,17 @@ import Movie.Movie;
 //configure the initial database value from the Main method
 
 public class CineplexDatabase implements Database{
+
+	private static Cineplex cathay;
+	private static Cineplex filmgarde;
+	private static Cineplex ShawTheatres;
 	
 	public static void main(String args[]){
 		StartCineplex();
 	}
 	
 	private static void StartCineplex(){
+		System.out.println("Start constructing database for Cineplex");
 		//Change code in here to configure the database 
 		//automatically configure 3 Cineplex from 1 company which is Cathat 
 		ArrayList<Cineplex> listCineplex = new ArrayList<Cineplex>();
@@ -40,7 +45,7 @@ public class CineplexDatabase implements Database{
 		/********************************************AMKHub******************************************************/
 		//each cineplex there has 3 cinema 
 		Cinema amkHubFirst = new Cinema("Room 1","Reguler");
-		Cinema amkHubSecond = new Cinema("Room 2","Velvet");
+		Cinema amkHubSecond = new Cinema("Room 2","Reguler");
 		Cinema amkHubThird = new Cinema("Room 3","Platinum");
 		
 		AMKHub.addCinema(amkHubFirst);
@@ -51,45 +56,43 @@ public class CineplexDatabase implements Database{
 		ArrayList<String> castList = new ArrayList<String>();
 		castList.add("Joseph Gordon-Levitt");
 		castList.add("Ben Kingsley");
-		Movie theWalk = new Movie("The walk","123 min","PG some intense sequence","Action",walkDate,10,"Robert Zemeckis",castList,
+		Movie theWalk = new Movie("The walk","123","PG some intense sequence","Action",walkDate,10,"Robert Zemeckis",castList,
 				"Twelve people have walked on the moon, but only one man has ever, or will ever, walk in the immense void between the World Trade Center towers."
 				+ " Guided by his real-life mentor, Papa Rudy (Ben Kingsley), and aided by an unlikely band of international recruits,"
 				+ " Petit and his gang overcome long odds, betrayals, dissension and countless close calls to conceive and execute their mad plan.");
 		
-		
 		AMKHub.addMovie(theWalk);
 		
+		
+		
+		
 		/*****************************************JEM**************************************************************/
-		Cinema firstCinemaJem = new Cinema("Room1");
-		Cinema secondCinemaJem = new Cinema("Room2");
-		Cinema thirdCinemaJem = new Cinema("Room3");
+		Cinema firstCinemaJem = new Cinema("Room1","Reguler");
+		Cinema secondCinemaJem = new Cinema("Room2","Platinum");
+		Cinema thirdCinemaJem = new Cinema("Room3","Reguler");
 				 
 		Jem.addCinema(firstCinemaJem);
 		Jem.addCinema(secondCinemaJem);
 		Jem.addCinema(thirdCinemaJem);
 		
-		DateMovie LastWitchHunterDate = new DateMovie(firstCinemaJem,"Not Showing",2015,10,10,13,20);
+		DateMovie LastWitchHunterDate = new DateMovie(firstCinemaJem,"Coming soon",2015,10,10,13,20);
 		ArrayList<String> castList1 = new ArrayList<String>();
 		castList.add("Vin Diesel");
 		castList.add("Elijah Wood");
-		Movie LastWitchHunter = new Movie("Last Witch Hunter","107 min","PG 13-some violence","Action",LastWitchHunterDate,10,"Breck Eisner",castList1,
+		Movie LastWitchHunter = new Movie("Last Witch Hunter","107","PG 13-some violence","Action",LastWitchHunterDate,10,"Breck Eisner",castList1,
 				"Five hundred years ago, war raged between humankind and witches, vicious creatures that unleashed a Black Death upon the world."
 				+ " Armies of witch hunters followed their leader, the great and mighty KAULDER, into a final battle that would bring the war to a close."
 				+ " In a rage, Kaulder managed to kill the all-powerful QUEEN WITCH and capture the mysterious source of her power, the Plague Box. "
-				+"However, in the moments before her death, the Queen cursed Kaulder with immortality. Forever separated from his beloved wife and daughter in the afterlife, "
-				+ "Kaulder has spent centuries hunting down rogue witches, all the while yearning for his long-lost loved ones."
-				+"Today, he is the only one of his kind—the last witch hunter. With the help of FATHER DOLAN, a Catholic priest, and a mystical arsenal hidden beneath New York’s St. "
-				+ "Patrick’s Cathedral, he continues to pursue the renegades and outlaws who endanger humanity. Chief among their adversaries is BELIAL,"
-				+ " a powerful rebel witch who Kaulder believes has found the long-hidden Plague Box and is preparing to use it to destroy humankind and restore the witches to power."
-				+ " With the help of CHLOE, a goodhearted young witch, Kaulder sets out to stop Belial before he can recover the final element he needs to set his plan in motion. "
-				+ "But the item Belial seeks is hidden in the last place anyone has thought to look…and seeming friends are not at all what they seem.");
+				+"However, in the moments before her death, the Queen cursed Kaulder with immortality");
+		
+		
+		
 		
 		Jem.addMovie(LastWitchHunter);
-		Jem.addMovie(LastWitchHunter);
-		Jem.addMovie(LastWitchHunter);
-		Jem.addMovie(LastWitchHunter);
-		Jem.addMovie(LastWitchHunter);
-		AMKHub.addMovie(LastWitchHunter);
+		
+
+		
+		
 		//implementation of the movie list will be added later by StaffApplication
 		
 		/*****************************************WestMall********************************************************/
@@ -102,18 +105,104 @@ public class CineplexDatabase implements Database{
 		WestMall.addCinema(secondWestMall);
 		WestMall.addCinema(thirdWestMall);
 		
+		
+		
+		//populate more movie into the database
+		
+		/**********************first movie populate in all cineplex***********************/
+		DateMovie paranomalActivityDate = new DateMovie(secondCinemaJem,"Coming soon",2015,12,12,15,30);
+		DateMovie paranomalActivityDate1 = new DateMovie(firstWestMall,"Coming soon",2015,12,12,15,30);
+		DateMovie paranomalActivityDate2 = new DateMovie(amkHubFirst,"Coming soon",2015,12,12,15,30);
+
+		ArrayList<String> castparanomalAct  = new ArrayList<String>();
+		castparanomalAct.add("Chris J. Murray");
+		castparanomalAct.add("Brit Shaw");
+		
+		Movie paranomalActivity = new Movie("Paranormal Activity The Ghost Dimension","95","NC16 - Horror and coarse language","Horror"
+		,paranomalActivityDate,10,"Gregory Plotkin",castparanomalAct,"The Ghost Dimension, follows a new family, The Fleeges - father Ryan (Chris J. Murray), mother Emily (Brit Shaw) and their young daughter Leila"
+				+ " (Ivy George) - Who move into a house and discover a video camera and a box of tapes in the garage. When "
+				+ "they look through the camera's lens, they begin to see the paranormal activity happening around them - including the re-emergence "
+				+ "of young Kristi and Katie.");
+		
+		
+		Movie paranomalActivity1 = new Movie("Paranormal Activity The Ghost Dimension","95","NC16 - Horror and coarse language","Horror"
+				,paranomalActivityDate1,10,"Gregory Plotkin",castparanomalAct,"The Ghost Dimension, follows a new family, The Fleeges - father Ryan (Chris J. Murray), mother Emily (Brit Shaw) and their young daughter Leila"
+						+ " (Ivy George) - Who move into a house and discover a video camera and a box of tapes in the garage. When "
+						+ "they look through the camera's lens, they begin to see the paranormal activity happening around them - including the re-emergence "
+						+ "of young Kristi and Katie.");
+				
+				
+				
+		Movie paranomalActivity2 = new Movie("Paranormal Activity The Ghost Dimension","95","NC16 - Horror and coarse language","Horror"
+				,paranomalActivityDate2,10,"Gregory Plotkin",castparanomalAct,"The Ghost Dimension, follows a new family, The Fleeges - father Ryan (Chris J. Murray), mother Emily (Brit Shaw) and their young daughter Leila"
+						+ " (Ivy George) - Who move into a house and discover a video camera and a box of tapes in the garage. When "
+						+ "they look through the camera's lens, they begin to see the paranormal activity happening around them - including the re-emergence "
+						+ "of young Kristi and Katie.");
+				
+				
+		AMKHub.addMovie(paranomalActivity);
+		Jem.addMovie(paranomalActivity1);
+		WestMall.addMovie(paranomalActivity2);
+		
+		/**********************second movie populate in all cineplex***********************/
+		
+		DateMovie gooseBumpsDate = new DateMovie(thirdCinemaJem,"Coming soon",2015,12,12,15,30);
+		DateMovie gooseBumpsDate1 = new DateMovie(secondWestMall,"Coming soon",2015,12,12,15,30);
+		DateMovie gooseBumpsDate2 = new DateMovie(amkHubThird,"Coming soon",2015,12,12,15,30);
+
+		ArrayList<String> castGooseBumps = new ArrayList<String>();
+		castGooseBumps.add("Jack Black");
+		castGooseBumps.add("Dylan Minnette Minnette");
+		
+		Movie gooseBumps = new Movie("Goosebumps","103","PG - Frightening scenes","BlockBuster"
+		,gooseBumpsDate,10,"Rob Letterman",castGooseBumps,"Upset about moving from a big city to a small town, teenager Zach Cooper (Dylan Minnette) "
+				+ "finds a silver lining when he meets the beautiful girl, Hannah (Odeya Rush), living right next door. But every silver lining has a cloud, "
+				+ "and Zach’s comes when he learns that Hannah has a mysterious dad who is revealed to be R. L. Stine (Jack Black), the author of the bestselling "
+				+ "Goosebumps series. It turns out that there is a reason why Stine is so strange… he is a prisoner of his own imagination");
+		
+		
+		Movie gooseBumps1 = new Movie("Goosebumps","103","PG - Frightening scenes","BlockBuster"
+		,gooseBumpsDate1,10,"Rob Letterman",castGooseBumps,"Upset about moving from a big city to a small town, teenager Zach Cooper (Dylan Minnette) "
+				+ "finds a silver lining when he meets the beautiful girl, Hannah (Odeya Rush), living right next door. But every silver lining has a cloud, "
+				+ "and Zach’s comes when he learns that Hannah has a mysterious dad who is revealed to be R. L. Stine (Jack Black), the author of the bestselling "
+				+ "Goosebumps series. It turns out that there is a reason why Stine is so strange… he is a prisoner of his own imagination");
+				
+				
+		Movie gooseBumps2 = new Movie("Goosebumps","103","PG - Frightening scenes","BlockBuster"
+		,gooseBumpsDate2,10,"Rob Letterman",castGooseBumps,"Upset about moving from a big city to a small town, teenager Zach Cooper (Dylan Minnette) "
+				+ "finds a silver lining when he meets the beautiful girl, Hannah (Odeya Rush), living right next door. But every silver lining has a cloud, "
+				+ "and Zach’s comes when he learns that Hannah has a mysterious dad who is revealed to be R. L. Stine (Jack Black), the author of the bestselling "
+				+ "Goosebumps series. It turns out that there is a reason why Stine is so strange… he is a prisoner of his own imagination");
+		
+		
+		
+		AMKHub.addMovie(gooseBumps);
+		Jem.addMovie(gooseBumps1);
+		WestMall.addMovie(gooseBumps2);
+		
+		
+		
 		//implementation of the movie list will be added later by Staff Application
 		
 		listCineplex.add(AMKHub);
 		listCineplex.add(Jem);
 		listCineplex.add(WestMall);
-		WestMall.addMovie(LastWitchHunter);
-		WestMall.addMovie(LastWitchHunter);
+		
+		//add all movie for rating purpose 
+		Movie lastWitchHunterRating = new Movie(LastWitchHunter.getTitle(),5);
+		Movie WalkRating			= new Movie(theWalk.getTitle(),8);
+		//test for double movie 
+		Movie LastWitchHunterRatingTest = new Movie(LastWitchHunter.getTitle(),10);
+		WestMall.addMovieForRating(lastWitchHunterRating);
+		WestMall.addMovieForRating(LastWitchHunterRatingTest);
+		WestMall.addMovieForRating(WalkRating);
 		
 		
 		CineplexDatabase cDatabase =  new CineplexDatabase();
-		System.out.println("size: "+listCineplex.size());
+		System.out.println("Finish Construct the original Database for Cineplex");
 		cDatabase.writeToDatabase("CineplexDatabase.dat", listCineplex);
+		CineplexDatabase cDatabase1 =  new CineplexDatabase();
+		ArrayList<Cineplex> listCineplex1 = cDatabase1.readFromDatabase("CineplexDatabase.dat");
 	
 	}
 	
@@ -150,4 +239,71 @@ public class CineplexDatabase implements Database{
 		}
 		return returnedList;
 	}
+	
+
+	
+	/*
+	 * No need to see this
+	 * 
+	 * 
+	 public void ConfigureCineplexForStaff(){
+		Scanner scan = new Scanner(System.in);
+		int choiceCineplex =  0;
+		//we are using 3 cineplex from Singapore 
+		//Cathay Cineplex
+		//Filmgarde
+		//Century Cineplex
+		
+		System.out.println("\n Please Choose the cineplex to configure:");
+		System.out.println("1)Cathay Cineplex");
+		System.out.println("2)Filmgarde");
+		System.out.println("3)Century Cineplex");
+		choiceCineplex = scan.nextInt();
+		switch(choiceCineplex){
+		case 1:
+			System.out.println("Start Configuring Cathay Cineplex");
+			if(cathay != null) currentCineplex = cathay;
+			break;
+		case 2:
+			System.out.println("Start Configuring Filmgarde");
+			if(filmgarde != null) currentCineplex = filmgarde;
+			break;
+		case 3:
+			System.out.println("Start Configuring Century Cineplex");
+			if(century != null) currentCineplex = century;
+			
+			break;
+		default:try{
+			throw new ChoiceException("Please choose another cineplex");
+		}catch(ChoiceException e){
+		
+		}
+		}
+	}
+	public void ConfiguringCinema(){
+		System.out.println("Configuring cinema from "+this.nameCineplex);
+		for(int i = 1; i <= currentCineplex.listCinema.size() ; i++){
+			System.out.printf("%d) %s \n",i,listCinema.get(i-1).nameCinema);
+		}
+		System.out.println("please choose one Cinema to configure\n");
+		Scanner scan = new Scanner(System.in);
+		int indexCinema = scan.nextInt();
+		ConfiguringMovie(indexCinema-1);
+		
+	}
+	
+	private void ConfiguringMovie(int pos){
+		currentCinema = listCinema.get(pos);
+		System.out.println("Configuring movie from "+currentCinema.nameCinema);
+		for(int i = 0 ; i < currentCinema.getArrayMovie().size() ; i++){
+			System.out.printf("%d) %s",i+1,currentCinema.getArrayMovie().get(i).nameMovie);
+		}
+		System.out.println("Please choose one Movie to configure");
+		Scanner scan = new Scanner(System.in);
+		int choiceMovie = scan.nextInt();
+		currentMovie = currentCinema.getArrayMovie().get(choiceMovie);
+	}
+	*/
+
+
 }
